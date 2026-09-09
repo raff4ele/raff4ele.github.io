05c9dbf6127ce533f37f6c3ef885ecc29c07b588
raff4ele <raff4.opi@gmail.com>
2026-09-09 21:40:41 +0200

    Load 3D engine as inline module so import maps work

--- js/atelier.js
+++ js/atelier.js
@@ -2,5 +2,7 @@ const [a, b] = await Promise.all([
   fetch(new URL("./atelier.p1.js", import.meta.url)).then((r) => r.text()),
   fetch(new URL("./atelier.p2.js", import.meta.url)).then((r) => r.text())
 ]);
-const url = URL.createObjectURL(new Blob([a, b], { type: "text/javascript" }));
-await import(url);
+const s = document.createElement("script");
+s.type = "module";
+s.textContent = a + "\n" + b;
+document.body.appendChild(s);
